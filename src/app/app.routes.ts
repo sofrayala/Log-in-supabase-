6071d5e1d6743ec9fc33f596f93289ae05479bf3
Sofia Ayala <sofiarayala1995@gmail.com>
2025-06-16 13:15:18 +0200

    log in ok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import AuthSignUpComponent from './auth/features/auth-sign-up/auth-sign-up.component';
 import AuthLogInComponent from './auth/features/auth-log-in/auth-log-in.component';
+import { NoteListComponent } from './notes/features/note-list/note-list.component';
 
 export const routes: Routes = [
   { path: 'sign-up', component: AuthSignUpComponent },
   { path: 'log-in', component: AuthLogInComponent },
-  { path: '**', component: AuthLogInComponent },
+  { path: '**', component: NoteListComponent },
+  { path: '', component: NoteListComponent },
 ];
