0166ae5610dd2495880d5b6a2a58882c1928ad78
Sofia Ayala <sofiarayala1995@gmail.com>
2025-06-27 10:56:16 +0200

    added pie, sparkline and doughnut charts

--- src/app/components/chart/chart.component.ts
+++ src/app/components/chart/chart.component.ts
@@ -20,6 +20,7 @@ import { Component, inject, effect } from '@angular/core';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
 import { BaseChartDirective } from 'ng2-charts';
 import { ChartType, ChartData } from 'chart.js';
+import { ChartOptions } from 'chart.js';
 import { NotesService } from '../../notes/data-access/notes.service';
 
 @Component({
@@ -30,6 +31,8 @@ import { NotesService } from '../../notes/data-access/notes.service';
 })
 export class ChartComponent {
   private notesService = inject(NotesService);
+
+  //bar
 
   chartType: ChartType = 'bar';
   chartData: ChartData<'bar'> = {
@@ -48,14 +51,101 @@ export class ChartComponent {
       },
     ],
   };
+
+  //sparkline
+  sparklineType: ChartType = 'line';
+  sparklineData: ChartData<'line'> = {
+    labels: [],
+    datasets: [
+      {
+        label: 'Rating Sparkline',
+        data: [],
+        borderColor: 'forestgreen',
+        backgroundColor: 'rgba(34,139,34,0.2)',
+        fill: true,
+        tension: 0.4,
+        pointRadius: 0,
+      },
+    ],
+  };
+  sparklineOptions: ChartOptions<'line'> = {
+    plugins: { legend: { display: false } },
+    elements: { line: { borderWidth: 2 } },
+    scales: { x: { display: false }, y: { display: false } },
+  };
+
+  //pie chart
+  pieChartData: ChartData<'pie'> = {
+    labels: [],
+    datasets: [
+      {
+        label: 'Ratings',
+        data: [],
+        backgroundColor: [
+          '#63BBF2',
+          '#63D8F2',
+          '#305912',
+          '#A1A60A',
+          '#F2785C',
+        ],
+      },
+    ],
+  };
+  pieChartOptions: ChartOptions<'pie'> = {
+    responsive: true,
+    plugins: {
+      legend: { display: true },
+    },
+  };
+
+  //dougnut
+  doughnutChartData: ChartData<'doughnut'> = {
+    labels: [],
+    datasets: [
+      {
+        label: 'Ratings',
+        data: [],
+        backgroundColor: [
+          '#63BBF2',
+          '#63D8F2',
+          '#305912',
+          '#A1A60A',
+          '#F2785C',
+        ],
+      },
+    ],
+  };
+  doughnutChartOptions: ChartOptions<'doughnut'> = {
+    responsive: true,
+    plugins: {
+      legend: { display: true },
+    },
+  };
+
   constructor() {
     this.notesService.getAllNotes();
 
     effect(() => {
       if (this.notesService.notesLoaded()) {
         const notes = this.notesService.notes();
-        this.chartData.labels = notes.map((note) => note.destination);
-        this.chartData.datasets[0].data = notes.map((note) => note.rating);
+        //bar info
+        const destinations = notes.map((note) => note.destination);
+        const ratings = notes.map((note) => note.rating);
+
+        this.chartData.labels = destinations;
+        this.chartData.datasets[0].data = ratings;
+
+        // Sparkline info
+        this.sparklineData.labels = destinations;
+        this.sparklineData.datasets[0].data = ratings;
+
+        //pie info
+        this.pieChartData.labels = destinations;
+        this.pieChartData.datasets[0].data = ratings;
+
+        //doughnut
+        this.doughnutChartData.labels = destinations;
+        this.doughnutChartData.datasets[0].data = ratings;
       }
     });
   }
